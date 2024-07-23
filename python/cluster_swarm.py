3ee9b0eda6e5f6ce20627a9d733e1eb26d549bc7
AureumIgnis <66358022+FDahlke@users.noreply.github.com>
2024-07-23 13:01:05 +0200

    -

---
+++ python/cluster_swarm.py
@@ -0,0 +1,69 @@
+from swarm import Swarm
+
+if __name__ == "__main__":
+
+
+    # Start the gazebo simulation
+    gz_process = subprocess.Popen(['gz', 'sim', '../worlds/example_swarm.sdf', '-r', '-s'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time.sleep(15)
+
+    #completed_process = subprocess.run('gz service -l', capture_output=True, shell=True)
+    #print(completed_process)
+
+    # Create the swarm object by passing the name
+    # of the world from the .sdf world file.
+    swarm = Swarm("example_swarm")
+
+    # Spawn 4 drones and keep the returning ids as handles
+    ids = swarm.spawn(4)
+
+    # First waypoints
+    waypoints = np.array([
+        [-2.0,-2.0, 30.0],
+        [ 2.0,-2.0, 30.0],
+        [-2.0, 2.0, 30.0],
+        [ 2.0, 2.0, 30.0],
+    ])
+
+    for d_z in range(0, 30, 5):
+
+        # Set the waypoints for all drones
+        current_waypoints = waypoints - np.array([0.0, 0.0, d_z])
+        swarm.waypoints(ids, current_waypoints)
+
+        # Wait until the data has arrived
+        time_delta = 0.01           # Delta time per sleep command in seconds
+        time_passed = 0.0           # Time counter to keep track of the time in seconds
+        timeout = 5.0               # Timeout in case something goes wrong
+
+        timeout_occured = False
+
+        while time_passed < timeout:
+
+            # All frames for a waypoint called send
+            # together, so it is enough to check the
+            # last one.
+            if swarm.received_frames[ids[-1]]:
+
+                for id in ids:
+                    rgb_image = swarm.rgb_images[id]
+                    thermal_image = swarm.thermal_images[id]
+                    depth_image = swarm.depth_images[id]
+                    # In this example we only store them in a data directory
+                    # OpenCV expects BGR but it is RGB, so switch the channels
+                    cv2.imwrite(f"../../data/swarm/rgb_image_drone_{id}_dz_{d_z}.png", cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB))
+                    cv2.imwrite(f"../../data/swarm/thermal_image_drone_{id}_dz_{d_z}.png", thermal_image)
+                    cv2.imwrite(f"../../data/swarm/depth_image_drone_{id}_dz_{d_z}.png", depth_image)
+                break
+
+            time.sleep(time_delta)
+            time_passed += time_delta
+        if time_passed >= timeout:
+                timeout_occured = True
+                print(f"Timeout reached for waypoints with dz={d_z}")
+
+    if timeout_occured:
+        raise TimeoutError("Timeout occured while waiting for waypoint")
+
+    subprocess.run(['pkill', '--full', 'gz sim server'])
+    subprocess.run(['pkill', '--full', 'gz sim gui'])
