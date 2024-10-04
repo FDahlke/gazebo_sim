1b7af9a4419c0156244ec3434fed88669e27841e
Florian D. <66358022+FDahlke@users.noreply.github.com>
2024-10-04 20:19:39 +0200

    fixed random movement

--- python/py100Forest.py
+++ python/py100Forest.py
@@ -156,9 +156,12 @@ def update_Target_Position_random(Target_Position):
     new_y = random.uniform(-1, 1)
             
     # Calculate the distance from the first coordinate
-    dist = distance(Target_Position, (new_x, new_y))
+    dist = distance(Target_Position, [new_x, new_y])
     random_vec = np.array([new_x,new_y])
-    return Target_Position+((random_vec/dist)*TARGET_STEPSIZE)
+    print(f"random vec{random_vec}, dist: {dist}")
+    newPos = Target_Position+((random_vec/dist)*TARGET_STEPSIZE)
+    print(f"New Pos: {newPos}")
+    return newPos
 
     
 def update_Target_Position_line(Target_Position):
