f7069aae6e97e977f867bb21d071a16780dc2239
Florian D. <66358022+FDahlke@users.noreply.github.com>
2024-10-15 10:40:53 +0200

    Update randomDrones.sh

--- python/randomDrones.sh
+++ python/randomDrones.sh
@@ -5,12 +5,15 @@ do
         offset=$(( 50*i ))
 		case $i in
 			0)
+			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5 slurm100mForest_Line_RandomDrones.slurm
 			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5 slurm100mForest_Line_RandomDrones.slurm
 			;;
 			1)
+			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10 slurm100mForest_Line_RandomDrones.slurm
 			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10 slurm100mForest_Line_RandomDrones.slurm
 			;;
 			2)
+			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20 slurm100mForest_Line_RandomDrones.slurm
 			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20 slurm100mForest_Line_RandomDrones.slurm
 			;;
 		esac
