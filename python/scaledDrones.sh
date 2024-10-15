690fa0851f0076ee601188f9b43dbb73f3fa03df
Florian D. <66358022+FDahlke@users.noreply.github.com>
2024-10-15 16:55:47 +0200

    -----

--- python/scaledDrones.sh
+++ python/scaledDrones.sh
@@ -3,7 +3,7 @@
 for (( i = 0; i < 3; ++i ))
 do
         offset=$(( 50*i ))
-		echo sbatch --export=ALL,PORT_OFFSET=${offset},MOVEMENT_TYPE=${i} slurm100mForest_Random_ScaledDrones
-		sbatch --export=ALL,PORT_OFFSET=${offset},MOVEMENT_TYPE=${i} slurm100mForest_Random_ScaledDrones
+		echo sbatch --export=ALL,PORT_OFFSET=${offset},MOVEMENT_TYPE=${i} slurm100mForest_Random_ScaledDrones.slurm
+		sbatch --export=ALL,PORT_OFFSET=${offset},MOVEMENT_TYPE=${i} slurm100mForest_Random_ScaledDrones.slurm
 done
 
