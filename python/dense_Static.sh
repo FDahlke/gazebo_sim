778281214c3d418ba74a17dbff87f95db9eb8072
FlorianDahlke <fdahlke@st.ovgu.de>
2024-10-17 17:18:34 +0200

    removed wrong logs

--- python/dense_Static.sh
+++ python/dense_Static.sh
@@ -6,15 +6,15 @@ do
 		case $i in
 			0)
 			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5, MOVEMENT_TYPE=0 slurmDenseForest.slurm
-			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5, MOVEMENT_TYPE=0 slurmDenseForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5,MOVEMENT_TYPE=0 slurmDenseForest.slurm
 			;;
 			1)
 			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10, MOVEMENT_TYPE=0 slurmDenseForest.slurm
-			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10, MOVEMENT_TYPE=0 slurmDenseForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10,MOVEMENT_TYPE=0 slurmDenseForest.slurm
 			;;
 			2)
 			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20, MOVEMENT_TYPE=0 slurmDenseForest.slurm
-			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20, MOVEMENT_TYPE=0 slurmDenseForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20,MOVEMENT_TYPE=0 slurmDenseForest.slurm
 			;;
 		esac
 done
