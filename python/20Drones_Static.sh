d20c6766eb77a675ddd31da78c0f366c9285b9c1
Florian D. <66358022+FDahlke@users.noreply.github.com>
2024-10-19 21:09:25 +0200

    added 20Drones_Static

---
+++ python/20Drones_Static.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for (( i = 0; i < 3; ++i ))
+do
+        offset=$(( 50*i ))
+		case $i in
+			0)
+			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5, MOVEMENT_TYPE=0 slurmSparseForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20,MOVEMENT_TYPE=0 slurmDenseForest.slurm
+			;;
+			1)
+			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10, MOVEMENT_TYPE=0 slurmSparseForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20,MOVEMENT_TYPE=0 slurmAverageForest.slurm
+			;;
+			2)
+			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20, MOVEMENT_TYPE=0 slurmSparseForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=20,MOVEMENT_TYPE=0 slurmSparseForest.slurm
+			;;
+		esac
+done
+
