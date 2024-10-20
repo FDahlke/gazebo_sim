67c01b5b04fd81b48d6229a4087dcbebcb1db184
Florian D. <66358022+FDahlke@users.noreply.github.com>
2024-10-20 11:02:35 +0200

    Update averageDrones.sh

--- python/averageDrones.sh
+++ python/averageDrones.sh
@@ -6,15 +6,15 @@ do
 		case $i in
 			0)
 			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10 slurmAverageForest.slurm
-			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10, MOVEMENT_TYPE=0 slurmSparseForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10,MOVEMENT_TYPE=0 slurmSparseForest.slurm
 			;;
 			1)
 			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10 slurmAverageForest.slurm
-			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10, MOVEMENT_TYPE=0 slurmAverageForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=10,MOVEMENT_TYPE=0 slurmAverageForest.slurm
 			;;
 			2)
 			echo running sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5 slurmAverageForest.slurm
-			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5, MOVEMENT_TYPE=0 slurmAverageForest.slurm
+			sbatch --export=ALL,PORT_OFFSET=${offset},NUM_DRONES=5,MOVEMENT_TYPE=0 slurmAverageForest.slurm
 			;;
 		esac
 done
